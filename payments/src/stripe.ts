c31cbe37a57e57d332f878150721aaf9ec6de125
ester.pratt <ester.pratt@naturalint.com>
2021-02-10 23:37:07 +0200

    log

--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
-console.log(process.env.STRIPE_KEY);
+console.log('stripe:', process.env.STRIPE_KEY);
 
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2020-08-27',
