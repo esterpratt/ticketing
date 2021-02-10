0c463492b68d2f3346b53b3ecc43da4e5667b0c3
ester.pratt <ester.pratt@naturalint.com>
2021-02-10 22:28:40 +0200

    test log

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -60,14 +60,11 @@ it('returns a 204 with valid inputs', async () => {
   });
   await order.save();
 
-  const response = await request(app)
+  await request(app)
     .post('/api/payments')
     .set('Cookie', global.signin(userId))
-    .send({ token: 'tok_visa', orderId: order.id });
-
-  console.log('res:', response);
-
-  expect(response.status).toEqual(201);
+    .send({ token: 'tok_visa', orderId: order.id })
+    .expect(201);
 
   const stripeCharges = await stripe.charges.list({ limit: 50 });
   const stripeCharge = stripeCharges.data.find((charge) => {
--- payments/src/stripe.ts
+++ payments/src/stripe.ts
@@ -1,5 +1,7 @@
 import Stripe from 'stripe';
 
+console.log(process.env.STRIPE_KEY);
+
 export const stripe = new Stripe(process.env.STRIPE_KEY!, {
   apiVersion: '2020-08-27',
 });
